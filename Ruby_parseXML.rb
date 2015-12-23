a50f19865fa6608ea769dbdfe41c2162ee4d9750
SSkyang <shengkangyang@gmail.com>
2015-12-23 22:43:10 +0800

    Update Ruby_parseXML.rb

--- Ruby_parseXML.rb
+++ Ruby_parseXML.rb
@@ -42,3 +42,127 @@ sec2 = root.elements[2]
 author = sec2.elements[1].elements["author"].text         #　Robert　Zubrin  
 puts author  
 puts "-- End --"  
+
+
+=begin
+ook.xml
+
+[xhtml] view plaincopy
+<library shelf="Recent Acquisitions">   
+<section name="Ruby">   
+<book isbn="0672328844">   
+<title>The Ruby Way</title>   
+<author>Hal Fulton</author>   
+<description>   
+Second edition. The book you are now reading.   
+Ain't recursion grand?   
+</description>   
+</book>   
+</section>   
+<section name="Space">   
+<book isbn="0684835509">   
+<title>The Case for Mars</title>   
+<author>Robert Zubrin</author>   
+<description>Pushing toward a second home for the human   
+race.   
+</description>   
+</book>   
+<book isbn="074325631X">   
+<title>First Man: The Life of Neil A. Armstrong</title>   
+<author>James R. Hansen</author>   
+<description>Definitive biography of the first man on   
+the moon. </description>   
+</book>   
+</section>   
+</library>  
+ 
+
+TestXML.xml
+
+[xhtml] view plaincopy
+<?xml version="1.0" encoding="utf-8"?>  
+<BoardComposant>  
+  <ComponantRectangle name="first" X="97" Y="186" Height="382" Width="587" >  
+    <JoinSpot>  
+      <Position>  
+        <X>147</X>  
+        <Y>219</Y>  
+      </Position>  
+      <Height>76</Height>  
+      <Width>86</Width>  
+    </JoinSpot>  
+    <JoinSpot>  
+      <Position>  
+        <X>510</X>  
+        <Y>236</Y>  
+      </Position>  
+      <Height>65</Height>  
+      <Width>77</Width>  
+    </JoinSpot>  
+    <JoinSpot>  
+      <Position>  
+        <X>194</X>  
+        <Y>380</Y>  
+      </Position>  
+      <Height>108</Height>  
+      <Width>83</Width>  
+    </JoinSpot>  
+    <JoinSpot>  
+      <Position>  
+        <X>417</X>  
+        <Y>400</Y>  
+      </Position>  
+      <Height>85</Height>  
+      <Width>167</Width>  
+    </JoinSpot>  
+  </ComponantRectangle>  
+    
+  <ComponantRectangle name="second" X="733" Y="179" Height="410" Width="343">  
+    <JoinSpot>  
+      <Position>  
+        <X>770</X>  
+        <Y>221</Y>  
+      </Position>  
+      <Height>78</Height>  
+      <Width>70</Width>  
+    </JoinSpot>  
+    <JoinSpot>  
+      <Position>  
+        <X>916</X>  
+        <Y>259</Y>  
+      </Position>  
+      <Height>68</Height>  
+      <Width>92</Width>  
+    </JoinSpot>  
+    <JoinSpot>  
+      <Position>  
+        <X>839</X>  
+        <Y>416</Y>  
+      </Position>  
+      <Height>109</Height>  
+      <Width>62</Width>  
+    </JoinSpot>  
+    <JoinSpot>  
+      <Position>  
+        <X>953</X>  
+        <Y>410</Y>  
+      </Position>  
+      <Height>85</Height>  
+      <Width>63</Width>  
+    </JoinSpot>  
+  </ComponantRectangle>  
+    
+  <ComponantRectangle name="third" X="126" Y="624" Height="282" Width="471">  
+    <JoinSpot>  
+      <Position>  
+        <X>179</X>  
+        <Y>658</Y>  
+      </Position>  
+      <Height>108</Height>  
+      <Width>133</Width>  
+    </JoinSpot>  
+  </ComponantRectangle>  
+    
+  <ComponantRectangle name="forth" X="333" Y="122" Height="282" Width="471" />  
+</BoardComposant>  
+=end
